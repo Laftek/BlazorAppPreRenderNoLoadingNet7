97bf571dbb686dc72e0486edfd75d29f312e921f
Laftek <balintmat@gmail.com>
2023-03-04 21:00:18 +0100

    more experimenting 3

--- cloudflare.build.sh
+++ cloudflare.build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet7
 ./dotnet7/dotnet --version
-./dotnet7/dotnet publish -c Release --property:PackageOutputPath=.
+./dotnet7/dotnet dotnet publish -c Release -o ${{env.DOTNET_ROOT}}/myapp -f=net7.0
